e3d8c2ead8756a1fcea6b0fc409034f565436243
Fábio Vieira Almeida <fabio.vieira.almeida@tecnico.ulisboa.pt>
2017-02-22 13:53:18 +0000

    small tweek to wander behaviour

--- scripts/brains/walterbrain.lua
+++ scripts/brains/walterbrain.lua
@@ -3,9 +3,16 @@ local WalterBrain = Class(Brain, function(self, inst)
 end)
 
 function WalterBrain:OnStart()
+	local times = {
+		minwalktime = 2,
+	    randwalktime = 3,
+	    minwaittime = 0,
+	    randwaittime = 1
+	}
+
     local root = PriorityNode(
     {
-    	Wander(self.inst, nil, 30)
+    	Wander(self.inst, nil, 30, times)
     }, 1)
     
 
