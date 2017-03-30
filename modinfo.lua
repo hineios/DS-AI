faa74f0679a2ed73d6e938a5bafb5ed0619189f9
Fábio Vieira Almeida <fabio.vieira.almeida@tecnico.ulisboa.pt>
2017-03-30 01:38:29 +0100

    updated version in order to update the workshop

--- modinfo.lua
+++ modinfo.lua
@@ -1,11 +1,11 @@
 -- Mod Settings
-name = "Artificial Wilson - Under Development"
-description = "Tired of playing the game? Let the game play for you! Click the brain to activate a smart AI capable of playing the game alone."
+name = "Artificial Wilson"
+description = "Tired of playing the game? Let the game play for you! Click the brain to activate a smart AI capable of playing the game alone. Click the stomach for a little cheaty bonus ;)"
 author = "Hineios"
-version = "0.1"
+version = "0.3"
 forumthread = "None"
--- icon_atlas = "modicon.xml"
--- icon = "modicon.tex"
+--icon_atlas = "modicon.xml"
+--icon = "modicon.tex"
 
 
 -- Compatibility
@@ -13,13 +13,4 @@ api_version = 10
 all_clients_require_mod = true
 dst_compatible = true
 
--- api_version = 10
--- api_version_dst = 10 
--- -- optional (if it's the same mod for DST and single-player)
--- dst_compatible = true
--- --This lets clients know if they need to get the mod from the Steam Workshop to join the game
--- all_clients_require_mod = true
--- --This is basically the opposite; it specifies that this mod doesn't affect other players at all, and if set, won't mark your server as modded
--- client_only_mod = false
--- --This lets people search for servers with this mod by these tags
 server_filter_tags = {"character", "other"}
